a5de80bd90e5f47a4c9f2147d41c43d8438fc00c
Ethan Burns <burns.ethan@gmail.com>
2010-04-25 12:31:53 -0400

    Add support for ocamlbuild.

---
+++ myocamlbuild.ml
@@ -0,0 +1,26 @@
+(** For including Cairo
+
+    @author eaburns
+    @since 2010-04-25
+*)
+
+open Ocamlbuild_plugin
+open Command
+
+let static = true;;
+
+dispatch begin function
+  | After_rules ->
+
+      (* If `static' is true then every ocaml link in bytecode will
+	 add -custom *)
+      if static then flag ["link"; "ocaml"; "byte"] (A"-custom");
+
+      flag ["link"; "ocaml"; "byte"; "use_cairo"]
+        (S[A "cairo.cma"; A"-cclib"; A"-lcairo"; ]);
+
+      flag ["link"; "ocaml"; "native"; "use_cairo"]
+        (S[A "cairo.cmxa"; A"-cclib"; A"-lcairo";]);
+
+  | _ -> ()
+end
