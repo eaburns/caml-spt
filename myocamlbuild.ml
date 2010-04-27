4719475886d23cd10acf7dadf8cdd6533bd34d57
Ethan Burns <burns.ethan@gmail.com>
2010-04-27 08:21:40 -0400

    Fix building with ocamlbuild.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -11,17 +11,10 @@ let static = true;;
 
 dispatch begin function
   | After_rules ->
-
       (* If `static' is true then every ocaml link in bytecode will
 	 add -custom *)
       if static then flag ["link"; "ocaml"; "byte"] (A"-custom");
 
       ocaml_lib ~extern:true ~dir:"+cairo" "cairo";
-(*       flag ["link"; "ocaml"; "byte"; "use_cairo"] *)
-(*         (S[A "cairo.cma"; A"-cclib"; A"-lcairo"; ]); *)
-
-(*       flag ["link"; "ocaml"; "native"; "use_cairo"] *)
-(*         (S[A "cairo.cmxa"; A"-cclib"; A"-lcairo";]); *)
-
   | _ -> ()
 end
