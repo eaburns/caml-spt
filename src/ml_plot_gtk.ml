e2278b0aaa08fc763e4739455c7912548076f746
Jordan Thayer <jordan.thayer@gmail.com>
2010-04-30 11:21:03 -0400

    I think the save dialog works now

--- src/ml_plot_gtk.ml
+++ src/ml_plot_gtk.ml
@@ -6,6 +6,23 @@ ignore (GtkMain.Main.init())
 
 let init_size = 400 (* default window size *)
 
+let file_dialog ~title ~callback () =
+  let sel =
+    GWindow.file_selection ~title ~modal:true (*?filename*) () in
+  ignore (sel#cancel_button#connect#clicked ~callback:sel#destroy);
+  ignore (sel#ok_button#connect#clicked ~callback:
+	    begin fun () ->
+	      let name = sel#filename in
+		sel#destroy ();
+		callback name
+	    end);
+  sel#show ()
+
+let save_dialog plot =
+    file_dialog ~title:"Save"
+      ~callback:(fun file ->
+		   Ml_plot_cairo.save plot file) ()
+
 let draw_plot_to_gtk_area plot area =
   (** [draw_plot plot area] draws the plot to a GTK drawing area. *)
   let ctx = Cairo_lablgtk.create area#misc#window in
@@ -18,18 +35,29 @@ let draw_plot_to_gtk_area plot area =
     plot#draw ctx
 
 
+open GdkKeysyms
+
 let create_display plot title =
   (** [create_display plot title] spawns a window with the given title
       showing the specified plot *)
   let width = init_size and height = init_size in
   let w = GWindow.window ~title ~width ~height () in
-  let area = GMisc.drawing_area ~width ~height ~packing:w#add() in
+  let vbox = GPack.vbox ~packing:w#add () in
+  let menu_bar = GMenu.menu_bar ~packing:vbox#pack () in
+  let factory = new GMenu.factory ~accel_path:"<MLPLOT>/" menu_bar in
+  let accel_group = factory#accel_group in
+  let file_menu = factory#add_submenu "File" in
+  let factory = new GMenu.factory ~accel_path:"<MLPLOT File>///" file_menu
+    ~accel_group in
+  let area = GMisc.drawing_area ~width ~height ~packing:vbox#pack() in
   let draw = draw_plot_to_gtk_area plot in
-    area#misc#realize ();
-    ignore (area#event#connect#expose (fun _ -> draw area; true));
-    ignore (w#connect#destroy GMain.quit);
-    ignore (draw area);
-    w#show();
-    GMain.main ()
+    ignore (factory#add_item "Save as..." ~key:_S ~callback:
+	      (fun _ -> (save_dialog plot)));
+      area#misc#realize ();
+      ignore (area#event#connect#expose (fun _ -> draw area; true));
+      ignore (w#connect#destroy GMain.quit);
+      ignore (draw area);
+      w#show();
+      GMain.main ()
 
 (* EOF *)
