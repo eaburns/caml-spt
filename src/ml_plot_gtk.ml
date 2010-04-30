08a0255baaaf32089f1686485fc6187f367fe7c1
Jordan Thayer <jordan.thayer@gmail.com>
2010-04-30 09:15:23 -0400

    Added ml_plot_gtk and stub file.  added comments at the top.

---
+++ src/ml_plot_gtk.ml
@@ -0,0 +1,4 @@
+(** Ml_plot_gtk - Holds all of the gtk display functionality for easy
+    removal*)
+
+(* EOF *)
---
+++ src/ml_plot_gtk_stub.ml
@@ -0,0 +1,5 @@
+(** Ml_plot_gtk_stub - Holds stubs for all of the gtk display functionality
+    everything does nothing and has a unit signature, allowing for easy
+    removal of gtk dependence *)
+
+(* EOF *)
