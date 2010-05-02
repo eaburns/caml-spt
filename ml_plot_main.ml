6b314a416c2b3fcb01b09cd232be2b1517723e10
Ethan Burns <burns.ethan@gmail.com>
2010-05-02 14:42:42 -0400

    Allow labels to be offset and located relative to the given point.

--- ml_plot_main.ml
+++ ml_plot_main.ml
@@ -44,23 +44,26 @@ let num_by_nom_plot () =
 
 let num_by_num_plot () =
   let next_dash = Num_by_num.default_dash_factory () in
-(*
   let next_glyph = Num_by_num.default_glyph_factory () in
-*)
   let next_line_err = Num_by_num.line_errbar_factory next_dash () in
     new Num_by_num.plot
       ~title:"Title text"
       ~xlabel:"X label text"
       ~ylabel:"Y label text"
       [
-	new Num_by_num.line_errbar_dataset
-	  ~name:"ds0" ~color:red (next_line_err ())
-	  [|
-	    [| point 0.5 0.5; point 1.0 1.0; point 2.0 2.0; point 10.0 3.5; |];
-	    [| point 8.5 4.5; point 1.8 7.7; point 5.7 2.6; point 20.7 7.5; |];
-	    [| point 3.2 1.6; point 4.7 2.1; point 3.6 7.7; point 10.0 0.7; |];
-	    [| point 1.9 7.5; point 9.2 7.0; point 8.5 7.1; point 10.1 3.9; |];
-	  |];
+	new Num_by_num.composite_dataset ~name:"ds0"
+	  [
+	    new Num_by_num.scatter_dataset (next_glyph ())
+	      [| point 0.5 0.5; point 1.0 1.0;
+		 point 2.0 2.0; point 10.0 3.5; |];
+	    new Num_by_num.label_dataset ~yoff:~-.0.013
+	      ~loc:Num_by_num.Label_above
+	      [| point 0.5 0.5, "one";
+		 point 1.0 1.0, "two";
+		 point 2.0 2.0, "three";
+		 point 10.0 3.5, "four";
+	      |];
+	  ];
 	new Num_by_num.line_errbar_dataset
 	  ~name:"ds1" ~color:green (next_line_err ())
 	  [|
