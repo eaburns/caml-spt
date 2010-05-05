36c470c77b08571c63d503e3329b1c44721cdf4b
Ethan Burns <burns.ethan@gmail.com>
2010-05-05 11:32:50 -0400

    Make a type for an axis.

--- src/num_by_num.ml
+++ src/num_by_num.ml
@@ -13,9 +13,9 @@ let axis_padding = 0.05
 (** {1 Numeric by numeric plot} ****************************************)
 
 class plot
-  ?(label_style=Spt.default_label_style)
-  ?(legend_style=Spt.default_legend_style)
-  ?(tick_style=Spt.default_tick_style)
+  ?(label_text_style=Spt.default_label_style)
+  ?(legend_text_style=Spt.default_legend_style)
+  ?(tick_text_style=Spt.default_tick_style)
   ?title ?xlabel ?ylabel
   ?(legend_loc=Legend.Lower_right)
   ?x_min ?x_max ?y_min ?y_max
@@ -58,24 +58,21 @@ object (self)
     Numeric_axis.tick_locations (yrange self#src_ranges)
 
 
-  method private dst_rectangle ctx ~src =
-    (** [dst_rectangle ctx ~src] get the dimensions of the destination
-	rectangle. *)
+  method private dst_rectangle ctx ~xaxis ~yaxis ~src =
+    (** [dst_rectangle ctx ~xaxis ~yaxis ~src] get the dimensions of
+	the destination rectangle. *)
     let xaspect, yaspect = self#aspect_ratio in
     let title_height =
       match title with
 	| None -> 0.
-	| Some txt -> snd (text_dimensions ctx ~style:label_style txt) in
+	| Some txt -> snd (text_dimensions ctx ~style:label_text_style txt) in
     let y_min', x_max' =
       Numeric_axis.resize_for_x_axis
-	ctx ~label_style ~tick_style ~pad:Spt.text_padding
-	~y_min:(yaspect -. axis_padding)
-	~src:(xrange src) ~dst:(range 0. xaspect)
-	xlabel self#xticks in
+	ctx ~pad:Spt.text_padding ~y_min:(yaspect -. axis_padding)
+	~dst:(range 0. xaspect) xaxis in
     let x_min' =
-      Numeric_axis.resize_for_y_axis ctx ~label_style ~tick_style
-	~pad:Spt.text_padding ~x_min:axis_padding
-	ylabel self#yticks in
+      Numeric_axis.resize_for_y_axis ctx
+	~pad:Spt.text_padding ~x_min:axis_padding yaxis in
     let dst =
       rectangle ~x_min:x_min' ~x_max:x_max' ~y_min:y_min'
 	~y_max:(title_height +. Spt.text_padding) in
@@ -93,50 +90,50 @@ object (self)
 	~y_max:(dst.y_max +. residual.y_max)
 
 
-  method private draw_x_axis ctx ~src ~dst =
-    (** [draw_x_axis ctx ~src ~dst] draws the
+  method private draw_x_axis ctx ~dst xaxis =
+    (** [draw_x_axis ctx ~dst xaxis] draws the
 	x-axis. *)
     let xaspect, yaspect = self#aspect_ratio in
-    Numeric_axis.draw_x_axis ctx
-      ~tick_style ~label_style ~pad:Spt.text_padding
-      ~width:xaspect ~height:yaspect
-      ~src:(xrange src) ~dst:(xrange dst)
-      xlabel self#xticks
+      Numeric_axis.draw_x_axis ctx ~pad:Spt.text_padding
+	~width:xaspect ~height:yaspect ~dst:(xrange dst) xaxis
 
 
-  method private draw_y_axis ctx ~src ~dst =
-    (** [draw_y_axis ctx ~src ~dst] draws the
-	y-axis. *)
+  method private draw_y_axis ctx ~dst yaxis =
+    (** [draw_y_axis ctx ~dst] draws the y-axis. *)
     let xaspect, yaspect = self#aspect_ratio in
-    Numeric_axis.draw_y_axis ctx
-      ~tick_style ~label_style ~pad:Spt.text_padding
-      ~width:xaspect ~height:yaspect
-      ~src:(yrange src) ~dst:(yrange dst)
-      ylabel self#yticks
+      Numeric_axis.draw_y_axis ctx
+	~pad:Spt.text_padding ~width:xaspect ~height:yaspect
+	~dst:(yrange dst) yaxis
 
 
   method draw ctx =
     let src = self#src_ranges in
-    let dst = self#dst_rectangle ctx ~src in
+    let xaxis =
+      Numeric_axis.create ~label_text_style ~tick_text_style
+	~src:(xrange src) self#xticks xlabel
+    and yaxis =
+      Numeric_axis.create ~label_text_style ~tick_text_style
+	~src:(yrange src) self#yticks ylabel in
+    let dst = self#dst_rectangle ctx ~xaxis ~yaxis ~src in
     let legend_txt_loc, legend_x, legend_y =
       let legend_dst = { dst with
 			   y_min = dst.y_min +. axis_padding;
 			   x_min = (dst.x_min -. axis_padding
 				    +. Spt.text_padding); }
-      in Legend.locate ctx legend_style legend_dst datasets legend_loc
+      in Legend.locate ctx legend_text_style legend_dst datasets legend_loc
     in
       begin match title with
 	| None -> ()
 	| Some t ->
 	    let x = (fst self#aspect_ratio) /. 2. and y = 0. in
-	      draw_text_centered_below ~style:label_style ctx x y t
+	      draw_text_centered_below ~style:label_text_style ctx x y t
       end;
-      self#draw_x_axis ctx ~src ~dst;
-      self#draw_y_axis ctx ~src ~dst;
+      self#draw_x_axis ctx ~dst xaxis;
+      self#draw_y_axis ctx ~dst yaxis;
       List.iter (fun ds -> ds#draw ctx ~src ~dst) datasets;
       save_transforms ctx;
       translate ctx legend_x legend_y;
-      Legend.draw ctx legend_txt_loc legend_style datasets;
+      Legend.draw ctx legend_txt_loc legend_text_style datasets;
       restore_transforms ctx
 
 end
