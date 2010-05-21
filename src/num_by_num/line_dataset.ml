44c75ce61c13168639d4f794bf13c5993199a166
Ethan Burns <burns.ethan@gmail.com>
2010-05-21 14:22:38 -0400

    Add beginning of box plots.

---
+++ src/num_by_num/line_dataset.ml
@@ -0,0 +1,126 @@
+(** Lines and lines with errorbars.
+
+    @author eaburns
+    @since 2010-04-28
+*)
+
+open Num_by_num_dataset
+open Drawing
+open Geometry
+
+let make_dash_factory dash_set () =
+  (** [make_dash_factory dash_set ()] makes a dash pattern factory. *)
+  let next = ref 0 in
+  let n = Array.length dash_set in
+    (fun () ->
+       let d = dash_set.(!next) in
+	 next := (!next + 1) mod n;
+	 d)
+
+let default_dash_factory =
+  (** [default_dash_factory] gets the default dash factory builder. *)
+  let default_dash_set =
+    [|
+      [| |];
+      [| Length.Pt 6.; Length.Pt 2.; |];
+      [| Length.Pt 2.; Length.Pt 2.; |];
+      [| Length.Pt 1.; Length.Pt 1.; |];
+      [| Length.Pt 5.; Length.Pt 2.; Length.Pt 1.; Length.Pt 2.; |];
+      [| Length.Pt 10.; Length.Pt 2.; Length.Pt 2.; Length.Pt 2.;
+	 Length.Pt 2.; Length.Pt 2.; Length.Pt 2.; Length.Pt 2.; |];
+      [| Length.Pt 10.; Length.Pt 2.; Length.Pt 2.; Length.Pt 2.; |];
+      [| Length.Pt 5.; Length.Pt 2.; Length.Pt 5.; Length.Pt 2.;
+	 Length.Pt 2.; Length.Pt 2.; Length.Pt 2.; Length.Pt 2.; |];
+      [| Length.Pt 4.; Length.Pt 2.; Length.Pt 4.; Length.Pt 1.;
+	 Length.Pt 1.; Length.Pt 1.; Length.Pt 1.; Length.Pt 1.;
+	 Length.Pt 1.; Length.Pt 1.; |];
+    |]
+  in make_dash_factory default_dash_set
+
+
+let line_legend_length = Length.Cm 0.75
+  (** The length of the line drawn in the legend. *)
+
+
+class line_dataset dashes ?(width=Length.Pt 1.) ?(color=black) ?name points =
+  (** A line plot dataset. *)
+object (self)
+  inherit points_dataset ?name points
+
+  val style =
+    {
+      line_color = color;
+      line_dashes = dashes;
+      line_width = width;
+    }
+
+  method draw ctx ~src ~dst =
+    let tr = rectangle_transform ~src ~dst in
+    let pts = ref [] in
+      for i = (Array.length points) - 1 downto 0 do
+	pts := (tr points.(i)) :: !pts
+      done;
+      draw_line ctx ~box:dst ~style !pts
+
+
+  method draw_legend ctx ~x ~y =
+    let half_length = (ctx.units line_legend_length) /. 2. in
+    let x0 = x -. half_length and x1 = x +. half_length in
+      draw_line ctx ~style [ point x0 y; point x1 y]
+
+
+  method legend_dimensions ctx =
+    (ctx.units line_legend_length), (ctx.units width)
+
+  method avg_slope =
+    let pts = points in
+      if Array.length pts < 2 then nan
+      else
+	(let accum = ref 0.
+	 and count = (Array.length pts - 2) in
+	   for i = 0 to count
+	   do
+	     (let pt1 = pts.(i)
+	      and pt2 = pts.(i+1) in
+	      let dy = pt2.y -. pt1.y
+	      and dx = pt2.x -. pt1.x in
+		accum := !accum +. (abs_float (dy /. dx)))
+	   done;
+	   !accum /. (float count))
+
+end
+
+let default_radius =
+  Length.Pt ((Length.as_pt Scatter_dataset.default_radius) /. 2.)
+
+
+let line_points_dataset dashes glyph ?(radius=default_radius)
+    ?width ?color ?name points =
+  (** [line_points_dataset dashes glyph ?radius ?width ?color ?name
+      points] makes a dataset that is a line with glyphs at each
+      point. *)
+  new composite_dataset ?name
+    [new line_dataset dashes ?width ?color ?name points;
+     new Scatter_dataset.scatter_dataset glyph ~radius ?color ?name points;]
+
+
+let line_dataset dashes ?width ?color ?name points =
+  new line_dataset dashes ?width ?color ?name points
+
+
+let line_datasets ?(uses_color=false) name_by_points_list =
+  let next_dash = default_dash_factory () in
+  List.map (fun (name, points) ->
+	      line_dataset (next_dash ()) ?name points)
+    name_by_points_list
+
+
+let line_points_datasets ?(uses_color=false) name_by_points_list =
+  let next_dash = default_dash_factory ()
+  and next_glyph = Factories.default_glyph_factory () in
+  List.map (fun (name, points) ->
+	      line_points_dataset (next_dash ()) (next_glyph()) ?name points)
+  name_by_points_list
+
+
+(* EOF *)
---
+++ src/numeric_axis.ml
@@ -0,0 +1,259 @@
+(** Drawing of a numeric axis.
+
+    @author eaburns
+    @since 2010-04-24
+*)
+
+open Printf
+open Geometry
+open Drawing
+
+type axis = {
+  src : range;
+  ticks : (float * string option) list;
+  label : string option;
+  label_text_style : text_style;
+  tick_text_style : text_style;
+}
+
+let axis_style =
+  (** The line style of a numeric axis. *)
+  {
+    line_color = black;
+    line_dashes = [| |];
+    line_width = Length.Pt 1.;
+  }
+
+
+let tick_length = Length.Pt 5.
+  (** The length of a tick mark. *)
+
+
+let tick_style =
+  (** The line style of a tick mark. *)
+  {
+    line_color = black;
+    line_dashes = [| |];
+    line_width = Length.Pt 0.5;
+  }
+
+let create ~label_text_style ~tick_text_style ~src ticks label =
+  (** [create ~label_text_style ~tick_text_style ~src ticks label]
+      creates a new axis. *)
+  {
+    label = label;
+    src = src;
+    ticks = ticks;
+    label_text_style = label_text_style;
+    tick_text_style = tick_text_style;
+  }
+
+
+let recommended_ticks length =
+  (** [recommended_ticks length] gets the recommended number of tick
+      marks. *)
+  let n = (Length.as_cm length) *. (1. /. 6.) in
+    if n < 2. then 2. else n
+
+
+(** {1 Tick marks} ****************************************)
+
+
+let rec tick_list major delta min max =
+  (** [tick_list major delta min max] gets a list of the tick
+      marks. *)
+  let fst = (floor (min /. delta)) *. delta in
+  let next = ref fst in
+  let lst = ref [] in
+    while !next < max do
+      let n = !next in
+      if n >= min && n <= max
+      then begin
+	let pv =
+	  let lg = truncate (log10 n) in if lg <= 0 then (~-lg + 1) else 0
+	in
+	let t = n, if major then Some (sprintf "%.*f" pv n) else None in
+	  lst := t :: !lst;
+      end;
+      next := delta +. n;
+    done;
+    !lst
+
+
+let tick_locations ?(suggested_number=2.) rng =
+  (** [tick_locations ?suggested_number rng] computes the location of
+      tick marks on a numeric axis with the given range. *)
+  let min = rng.min and max = rng.max in
+  let group_width = (max -. min) /. suggested_number in
+  let tens = 10. ** (floor (log10 group_width)) in
+  let ntens = (max -. min) /. tens in
+  let delta = (floor (ntens /. suggested_number)) *. tens in
+  let major_ticks = tick_list true delta min max in
+  let minor_ticks =
+    List.filter (fun (minor_vl, _) ->
+		   List.exists (fun (major_vl, _) -> major_vl <> minor_vl)
+		     major_ticks)
+      (tick_list false (delta /. 2.) min max) in
+    major_ticks @ minor_ticks
+
+
+let max_tick_text_width ctx style ticks =
+  (** [max_tick_text_width ctx style ticks] gets the maximum width of
+      the text for the given ticks. *)
+  List.fold_left (fun m (_, txt_opt) ->
+		    match txt_opt with
+		      | None -> m
+		      | Some txt ->
+			  let w = fst (text_dimensions ctx ~style txt) in
+			    if w > m then w else m)
+    0. ticks
+
+let max_tick_text_height ctx style ticks =
+  (** [max_tick_text_height ctx style ticks] gets the maximum height of
+      the text for the given ticks. *)
+  List.fold_left (fun m (_, txt_opt) ->
+		    match txt_opt with
+		      | None -> m
+		      | Some txt ->
+			  let h = snd (text_dimensions ctx ~style txt) in
+			    if h > m then h else m)
+    0. ticks
+
+
+(** {1 Drawing an x-axis} ****************************************)
+
+let resize_for_x_axis ctx ~pad ~y_min ~dst axis =
+  (** [resize_for_x_axis ctx ~pad ~y_min ~dst axis] gets the new scale
+      after making room for the x-axis tick marks and label.  [pad] is
+      the padding between text.  The result is a new (y_min * x_max)
+      that will have room for the x-axis and the x-tick label text. *)
+  let tick_length = ctx.units tick_length in
+  let tick_text_style = axis.tick_text_style in
+  let label_room =
+    match axis.label with
+      | None -> 0.
+      | Some label ->
+	  snd (text_dimensions ctx ~style:axis.label_text_style label)
+  in
+  let ticks_with_text = List.filter (fun (_, t) -> t <> None) axis.ticks in
+  let x_tick_txt_height =
+    match ticks_with_text with
+      | (_,  Some txt) :: tl ->
+	  snd (text_dimensions ctx ~style:tick_text_style txt)
+      | _ -> 0. in
+  let y_min' =
+    y_min
+    -. label_room -. pad
+    -. tick_length -. pad -. x_tick_txt_height
+  in
+  let over (vl, txt_opt) =
+    match txt_opt with
+      | None -> 0.
+      | Some txt ->
+	  let width, _ = text_dimensions ctx ~style:tick_text_style txt in
+	  let tr = range_transform ~src:axis.src ~dst in
+	  let x = tr vl in
+	  let over = (x +. (width /. 2.)) -. dst.max in
+	    if over > 0. then over else 0.
+  in
+  let max_over =
+    (* Check how far each tick text on the x-axis goes over the
+       maximum x-value so that this can be pushed back to fit the
+       text. *)
+    List.fold_left (fun max tick ->
+		      let o = over tick in
+			if o > max then o else max)
+      0. axis.ticks
+  in
+    y_min', dst.max -. max_over
+
+
+let draw_x_tick ctx style ~pad ~y scale (vl, t_opt) =
+  (** [draw_x_tick ctx style ~pad ~y scale t] draws an x-tick with the
+      top at the given [y] location.. *)
+  let tick_length = ctx.units tick_length in
+  let x = scale vl in
+  let len = if t_opt = None then tick_length /. 2. else tick_length in
+    draw_line ctx ~style:tick_style [ point x y; point x (y +. len) ];
+    begin match t_opt with
+      | Some txt ->
+	  draw_text_centered_below ctx ~style x (y +. len +. pad) txt;
+      | None -> ()
+    end
+
+
+let draw_x_axis ctx ~pad ~width ~height ~dst axis =
+  (** [draw_x_axis ctx ~pad ~width ~height ~dst axis] draws an
+      x-axis. [scale] is a function that converts an x-value in the
+      original data coordinates to the destination x-coordinate
+      system. *)
+  let tick_length = ctx.units tick_length in
+  let tick_text_height =
+    max_tick_text_height ctx axis.tick_text_style axis.ticks in
+  let tr = range_transform ~src:axis.src ~dst in
+  let h = match axis.label with
+    | None -> 0.
+    | Some label ->
+	let h = snd (text_dimensions ctx ~style:axis.label_text_style label) in
+	let x = width /. 2. in
+	  draw_text_centered_above ctx x height label;
+	  h
+  in
+  let y' = height -. h -. pad -. tick_text_height -. pad -. tick_length in
+    List.iter (draw_x_tick ctx axis.tick_text_style ~pad ~y:y' tr) axis.ticks;
+    draw_line ctx ~style:axis_style [ point dst.min y'; point dst.max y'; ]
+
+
+(** {1 Drawing a y-axis} ****************************************)
+
+let resize_for_y_axis ctx ~pad ~x_min axis =
+  (** [resize_for_y_axis ctx ~pad ~x_min axis] gets the new minimum
+      and maximum x-values after making room for the y-axis tick marks
+      and label.  [pad] is the padding between text. *)
+  let tick_length = ctx.units tick_length in
+  let label_room =
+    match axis.label with
+      | None -> 0.
+      | Some label ->
+	  snd (text_dimensions ctx ~style:axis.label_text_style label)
+  in
+  let tick_text_width =
+    max_tick_text_width ctx axis.tick_text_style axis.ticks
+  in
+    x_min
+    +. label_room +. pad
+    +. tick_length +. pad +. tick_text_width
+
+
+let draw_y_tick ctx style ~pad ~x tr (vl, t_opt) =
+  (** [draw_y_tick ctx style ~pad ~x tr t]
+      draws a y-tick with the left at the given [x] location.. *)
+  let tick_length = ctx.units tick_length in
+  let y = tr vl in
+  let len = if t_opt = None then tick_length /. 2. else tick_length in
+    draw_line ctx ~style:tick_style [ point x y; point (x -. len) y ];
+    begin match t_opt with
+      | Some txt ->
+	  draw_text_centered_before ctx ~style (x -. len -. pad) y txt;
+      | None -> ()
+    end
+
+
+let draw_y_axis ctx ~pad ~width ~height ~dst axis =
+  (** [draw_y_axis ctx ~pad ~width ~height ~dst axis] draws a
+  y-axis. *)
+  let tick_length = ctx.units tick_length in
+  let tick_text_width =
+    max_tick_text_width ctx axis.tick_text_style axis.ticks in
+  let tr = range_transform ~src:axis.src ~dst in
+  let h = match axis.label with
+    | None -> 0.
+    | Some label ->
+	let h = snd (text_dimensions ctx ~style:axis.label_text_style label) in
+	let y = height /. 2. in
+	  draw_text ctx ~angle:270. (h /. 2.) y label;
+	  h
+  in
+  let x' = h +. pad +. tick_text_width +. pad +. tick_length in
+    List.iter (draw_y_tick ctx axis.tick_text_style ~pad ~x:x' tr) axis.ticks;
+    draw_line ctx ~style:axis_style [ point x' dst.min; point x' dst.max; ]
