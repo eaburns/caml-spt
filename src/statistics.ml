9b9a55825d41588f6521920273de315fc9551cdd
Ethan Burns <burns.ethan@gmail.com>
2010-05-26 09:18:39 -0400

    Remove an error in spt_main.ml

---
+++ src/statistics.ml
@@ -0,0 +1,88 @@
+(** Some statistic functions.
+
+    @author eaburns
+    @since 2010-04-28
+*)
+
+type float_ref = { mutable v : float }
+
+let float_ref x = { v = x }
+
+let (<--) r v = r.v <- v
+
+let (!!) r = r.v
+
+let mean vls =
+  (** [mean vls] gets the mean of an array of floats. *)
+  let n = float (Array.length vls) in
+  let sum = float_ref 0. in
+    Array.iter (fun v -> sum <-- !!sum +. v) vls;
+    !!sum /. n
+
+
+let mean_and_stdev vls =
+  (** [mean_and_stdev vls] gets the mean and standard deviation of an
+      array of floats. *)
+  let n = float (Array.length vls) in
+  let sum = float_ref 0. in
+    Array.iter (fun v -> sum <-- !!sum +. v) vls;
+    let mean = !!sum /. n in
+    let diffs = float_ref 0. in
+      Array.iter (fun v -> diffs <-- !!diffs +. (v -. mean) ** 2.) vls;
+      mean, sqrt (!!diffs /. n)
+
+
+let mean_and_interval vls =
+  (** [mean_and_interval vls] gives the mean and the magnitude of the
+      95% confidence interval on the mean. *)
+  let mu, sigma = mean_and_stdev vls in
+  let n = float (Array.length vls) in
+    mu, 1.96 *. sigma /. (sqrt n)
+
+
+let percentile p vls =
+  (** [percentile p vls] computes the [p] percentile of the values
+      [vls] by ranking them.
+
+      According to wikipedia, this procedure is recommended by the
+      National Institute of Standards and Technology (NIST). *)
+  if p < 0. || p > 100. then invalid_arg "percentile: out of bounds";
+  let cmp (a : float) b = if a < b then ~-1 else if a > b then 1 else 0 in
+  let ranked = Array.copy vls in
+  let num = float (Array.length ranked) in
+  let n = p *. (num -. 1.) /. 100. +. 1. in
+  let k = truncate n in
+  let d = n -. (float k) in
+    Array.sort cmp ranked;
+    match n with
+      | n when n <= 1. -> ranked.(0)
+      | n when n >= num -> ranked.((truncate num) - 1)
+      | n -> ranked.(k - 1) +. d *. (ranked.(k) -. ranked.(k - 1))
+
+
+let upper_and_lower_fence vls =
+  (** [upper_and_lower_fence vls] computes the upper and lower fence.
+      These values are used to determine what points may be considered
+      outliers.  upper = Q2 + 4 * (Q3 - Q2) and lower = Q2 + 4 * (Q1 -
+      Q2) *)
+  let q1 = percentile 25. vls
+  and q2 = percentile 50. vls
+  and q3 = percentile 75. vls
+  and k = 4. in
+  (q2 +. k *. (q3 -. q2),
+   q2 +. k *. (q1 -. q2))
+
+
+let separate_outliers vls =
+  (** [separate_outliers vls] separates the outliers that are outside
+      the upper and lower fence values. *)
+  let upper, lower = upper_and_lower_fence vls in
+  let out_lst, dat_lst =
+    Array.fold_left (fun (os, ds) v ->
+		       if v > upper || v < lower
+		       then v :: os, ds
+		       else os, v :: ds)
+      ([], []) vls
+  in Array.of_list out_lst, Array.of_list dat_lst
+
+(* EOF *)
