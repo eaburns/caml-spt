804482c311c2cb1f0b13e3d07115783b80fd2d77
Ethan Burns <burns.ethan@gmail.com>
2010-06-07 09:20:21 -0400

    Better clipping of boxplots (not done).

--- src/errbar.ml
+++ src/errbar.ml
@@ -93,15 +93,16 @@ let draw_down ctx ?(style=errbar_line_style) ~src ~dst ~x ~y ~mag =
       rectangle. *)
   let errbar_cap_size = ctx.units errbar_cap_size in
   let tr = range_transform ~src ~dst in
-  let y0' = tr y and y1 = y -. mag in
-  let y1', clip =
-    if y1 < src.min then tr src.min, true else tr y1, false
-  in
-    draw_line ctx ~style [point x y0'; point x y1'];
-    if not clip
+  let y1 = y -. mag in
+  let y1_clipped = y1 > src.max || y1 < src.min in
+    if y >= src.min && y1 < src.max
     then begin
-      draw_line ctx ~style [ point (x -. errbar_cap_size) y1';
-			     point (x +. errbar_cap_size) y1' ]
+      let y0' = tr y  in
+      let y1' = if y1 < src.min then tr src.min else tr y1 in
+	draw_line ctx ~style [point x y0'; point x y1'];
+	if not y1_clipped
+	then draw_line ctx ~style [ point (x -. errbar_cap_size) y1';
+				    point (x +. errbar_cap_size) y1' ]
     end
 
 
