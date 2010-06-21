28428c40048bd35116c75aee830ffafb63472d4f
Ethan Burns <burns.ethan@gmail.com>
2010-06-21 09:19:22 -0400

    Add some functions over data.

--- src/spt-it-out/eval_data.ml
+++ src/spt-it-out/eval_data.ml
@@ -46,7 +46,25 @@ let read_triples inch =
       Triples (Array.of_list (List.rev !triples))
 
 
+
 (** {1 Convenience} ****************************************)
+
+let scalars eval_rec env exp = match eval_rec env exp with
+  | Scalars p -> p
+  | x -> failwith (sprintf "line %d: Expected scalars" (Sexpr.line_number exp))
+
+
+let points eval_rec env exp = match eval_rec env exp with
+  | Points p -> p
+  | x -> failwith (sprintf "line %d: Expected points" (Sexpr.line_number exp))
+
+
+let rec triples eval_rec env exp = match eval_rec env exp with
+  | Triples p -> p
+  | x -> failwith (sprintf "line %d: Expected scalars" (Sexpr.line_number exp))
+
+
+(** {1 Functions} ****************************************)
 
 let eval_scalars eval_rec env line operands =
   (** [eval_scalars eval_rec env line operands] evaluates a scalar
@@ -229,6 +247,123 @@ divisable by three.  Each set of three values is taken to be a\n\
 triple i, j, k."
 
 
+let eval_log10 eval_rec env line = function
+  | expr :: [] ->
+      begin match eval_rec env expr with
+	| Scalars ss -> Scalars (Array.map log10 ss)
+	| Points ps ->
+	    Points (Array.map (fun p -> point (log10 p.x) (log10 p.y)) ps)
+	| Triples ts ->
+	    Triples
+	      (Array.map (fun t ->
+			    triple (log10 t.i) (log10 t.j) (log10 t.k))
+		 ts)
+	| x ->
+	    printf
+	      "line %d: Expected one of: Scalars, Points or Triples, got %s\n"
+	      (Sexpr.line_number expr) (value_to_string x);
+	    raise (Invalid_argument (Sexpr.line_number expr))
+      end
+  | _ ->
+      printf "line %d: Expected scalars\n" line;
+      raise (Invalid_argument line)
+
+
+let help_str_log10 =
+  "(log10 [<scalars>|<points>|<triples>])\n\
+Gets the log base 10 of the values.  If the argument is a composite\n\
+(points or triples) then the log is applied to all of the fields."
+
+
+let eval_fst eval_rec env line = function
+  | expr :: [] ->
+      begin match eval_rec env expr with
+	| Points ps -> Scalars (Array.map (fun p -> p.x) ps)
+	| Triples ts -> Scalars (Array.map (fun t -> t.i) ts)
+	| x ->
+	    printf "line %d: Expected one of: Points or Triples, got %s\n"
+	      (Sexpr.line_number expr) (value_to_string x);
+	    raise (Invalid_argument (Sexpr.line_number expr))
+      end
+  | _ -> raise (Invalid_argument line)
+
+
+let help_str_fst =
+  "(fst [<points>|<triples>])\nGets the first value of a composite."
+
+
+let eval_snd eval_rec env line = function
+  | expr :: [] ->
+      begin match eval_rec env expr with
+	| Points ps -> Scalars (Array.map (fun p -> p.y) ps)
+	| Triples ts -> Scalars (Array.map (fun t -> t.j) ts)
+	| x ->
+	    printf "line %d: Expected one of: Points or Triples, got %s\n"
+	      (Sexpr.line_number expr) (value_to_string x);
+	    raise (Invalid_argument (Sexpr.line_number expr))
+      end
+  | _ -> raise (Invalid_argument line)
+
+
+let help_str_snd =
+  "(snd [<points>|<triples>])\nGets the second value of a composite."
+
+
+let eval_thrd eval_rec env line = function
+  | expr :: [] ->
+      begin match eval_rec env expr with
+	| Triples ts -> Scalars (Array.map (fun t -> t.k) ts)
+	| x ->
+	    printf "line %d: Expected Triples, got %s\n"
+	      (Sexpr.line_number expr) (value_to_string x);
+	    raise (Invalid_argument (Sexpr.line_number expr))
+      end
+  | _ -> raise (Invalid_argument line)
+
+
+let help_str_thrd =
+  "(thrd <triples>)\nGets the third value of a composite."
+
+let eval_combine eval_rec env line = function
+  | expa :: expb :: [] ->
+      let sa = scalars eval_rec env expa
+      and sb = scalars eval_rec env expb in
+      let n = Array.length sa in
+	if (Array.length sb) <> n
+	then begin
+	  printf "line %d: Arguments not of the same size\n" line;
+	  raise (Invalid_argument line)
+	end;
+	let pts = Array.create n (point 0. 0.) in
+	  for i = 0 to n - 1 do
+	    pts.(i) <- point sa.(i) sb.(i);
+	  done;
+	  Points pts
+  | expa :: expb :: expc :: [] ->
+      let sa = scalars eval_rec env expa
+      and sb = scalars eval_rec env expb
+      and sc = scalars eval_rec env expb in
+      let n = Array.length sa in
+	if (Array.length sb) <> n || (Array.length sc) <> n
+	then begin
+	  printf "line %d: Arguments not of the same size\n" line;
+	  raise (Invalid_argument line)
+	end;
+	let trps = Array.create n (triple 0. 0. 0.) in
+	  for i = 0 to n - 1 do
+	    trps.(i) <- triple sa.(i) sb.(i) sc.(i);
+	  done;
+	  Triples trps
+  | x :: _ -> raise (Invalid_argument (Sexpr.line_number x))
+  | [] -> raise (Invalid_argument line)
+
+
+let help_str_combine =
+  "(combine [<scalars> <scalars>|<scalars> <scalars> <scalars>])\n\
+Combines two sets of scalars into a set of points or three sets\n\
+of scalars into a set of triples."
+
+
 let functions = [
   "scalars", eval_scalars, help_str_scalars;
   "scalars-file", eval_scalars_file, help_str_scalars_file;
@@ -239,20 +374,10 @@ let functions = [
   "triples", eval_triples, help_str_triples;
   "triples-file", eval_triples_file, help_str_triples_file;
   "triples-cmd", eval_triples_cmd, help_str_triples_cmd;
+
+  "log10", eval_log10, help_str_log10;
+  "fst", eval_fst, help_str_fst;
+  "snd", eval_snd, help_str_snd;
+  "thrd", eval_thrd, help_str_thrd;
+  "combine", eval_combine, help_str_combine;
 ]
-
-(** {1 Convenience} ****************************************)
-
-let scalars eval_rec env exp = match eval_rec env exp with
-  | Scalars p -> p
-  | x -> failwith (sprintf "line %d: Expected scalars" (Sexpr.line_number exp))
-
-
-let points eval_rec env exp = match eval_rec env exp with
-  | Points p -> p
-  | x -> failwith (sprintf "line %d: Expected points" (Sexpr.line_number exp))
-
-
-let rec triples eval_rec env exp = match eval_rec env exp with
-  | Triples p -> p
-  | x -> failwith (sprintf "line %d: Expected scalars" (Sexpr.line_number exp))
