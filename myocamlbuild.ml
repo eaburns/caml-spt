3c64eda868368bbb7c69fdacbf61a57243788cf4
Ethan Burns <burns.ethan@gmail.com>
2010-06-22 12:30:11 -0400

    Fix more bugs in the myocamlbuild.ml plugin.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,4 +1,4 @@
-(** For including Cairo
+(** OCaml build plugin to include the necessary external libs.
 
     @author eaburns
     @since 2010-04-25
@@ -6,6 +6,8 @@
 
 open Ocamlbuild_plugin
 open Command
+
+;;
 
 dispatch begin function
   | After_rules ->
@@ -16,8 +18,7 @@ dispatch begin function
       ocaml_lib ~extern:true ~dir:"+lablgtk2" "lablgtk";
 
       ocaml_lib ~extern:true ~dir:"+lacaml" "lacaml";
-      (* link with fortran when using lacaml. *);
+      (* link with fortran when using lacaml. *)
       flag ["link"; "ocaml"; "use_lacaml"] (S[A "-cclib"; A "-lgfortran" ]);
-
   | _ -> ()
 end
